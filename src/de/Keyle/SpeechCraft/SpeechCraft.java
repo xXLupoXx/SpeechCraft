d8e4378a154b7bae1b1ec2e253832d9a7b5378b0
Keyle <keyle@t-online.de>
2011-07-04 22:30:45 +0200

    clean up the initial code

--- src/de/Keyle/SpeechCraft/SpeechCraft.java
+++ src/de/Keyle/SpeechCraft/SpeechCraft.java
@@ -3,8 +3,6 @@ package de.Keyle.SpeechCraft;
 
 import de.Keyle.SpeechCraft.commands.*;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -24,7 +22,7 @@ public class SpeechCraft extends JavaPlugin
 	public void onEnable()
 	{       
         PluginDescriptionFile pdfFile = this.getDescription();
-        System.out.println( "["pdfFile.getName() + "] version " + pdfFile.getVersion() + " is enabled!" );
+        System.out.println( "[" + pdfFile.getName() + "] version " + pdfFile.getVersion() + " is enabled!" );
 
         getCommand("lang").setExecutor(new SetLanguage(this));
     }
